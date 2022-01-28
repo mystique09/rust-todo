d74f32646a91e3f52dcb022067247ea9910eb439
mystique09 <knightkrypto7@gmail.com>
2022-01-28 10:03:55 +0800

    add get,all,new,del, and update handlers

--- src/routes/user_router.rs
+++ src/routes/user_router.rs
@@ -1,27 +1,36 @@
-use crate::models::user_model::User;
+use crate::models::user_model::{CreateUser, CreationError, UpdateUser, User};
 use axum::{
     extract::Path,
     response::{IntoResponse, Json},
 };
-
-pub async fn all_user() -> impl IntoResponse {
-    let test_user: Vec<User> = vec![User {
-        id: 1,
-        username: "Benjie".to_string(),
-        email: "testemail".to_string(),
-        password: "testpass".to_string(),
-        role: "Normal".to_string(),
-    }];
-    Json(test_user)
+pub async fn get_users_rt() -> impl IntoResponse {
+    let result = User::get_users();
+    Json(result)
 }
 
-pub async fn user_by_id(Path(id): Path<i32>) -> impl IntoResponse {
-    let user = User {
-        id,
-        username: "Test".to_string(),
-        password: "test".to_string(),
-        email: "test".to_string(),
-        role: "Normal".to_string(),
-    };
-    Json(user)
+pub async fn get_user_rt(Path(id): Path<i32>) -> impl IntoResponse {
+    let result = User::get_user(id);
+    Json(result)
+}
+
+pub async fn new_user_rt(Json(create_user): Json<CreateUser>) -> impl IntoResponse {
+    let user = User::new_user(create_user);
+    match user {
+        Ok(_user) => Json("New user added."),
+        Err(CreationError::DuplicateKey(err)) => Json(err),
+        Err(_) => Json("Something went wron"),
+    }
+}
+
+pub async fn update_user_rt(
+    Path(_id): Path<i32>,
+    Json(_update_user): Json<UpdateUser>,
+) -> impl IntoResponse {
+    //todo!();
+    Json("unimplemented!()")
+}
+
+pub async fn delete_user_rt(Path(_id): Path<i32>) -> impl IntoResponse {
+    //todo!();
+    Json("unimplemented!()")
 }
